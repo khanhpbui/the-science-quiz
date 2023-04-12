53e2fc561109b54e9d69ce09405ddc273ded231a
Khanh Bui <pkkhanhbui@gmail.com>
2023-04-12 08:44:20 -0400

    worked on css for better UI

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
-# javascript-quiz
+# The Science Quiz
+
+## Table of Contents:
+---
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Screenshot](#screenshot)
+- [Link](#link)
+- [Contact](#contact)
 
 ## Description
-
-This is a timed quiz to test the bootcamp student's basic knowledge of Javascript.
-
-
+---
+Behold, the ultimate test of your scientific prowess - the science quiz! Put on your lab coat and goggles, and prepare to be grilled with brain-busting questions that will leave you questioning your own intelligence. Get ready to laugh, cry, and maybe even scream in frustration as you attempt to answer questions about everything from quantum mechanics to the mating habits of fruit flies. So, are you ready to take the plunge into the wild and wacky world of science? Let's find out!
 
 
 ## Installation
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,171 +1,171 @@
 var finalScore = 0;
 var secondsLeft = 50;
 var counter = 0;
-var viewScoreBtnEl = document.querySelector('#view-score');
-var timeEl = document.querySelector('#time-el');
-var startBtnEl = document.querySelector('#start-btn');
-var questionEl = document.querySelector('#question');
-var optA = document.querySelector('#a');
-var optB = document.querySelector('#b');
-var optC = document.querySelector('#c');
-var optD = document.querySelector('#d');
-var answerEl = document.querySelector('#answer');
-var allDone = document.querySelector('#all-done');
-var finalScoreEl = document.querySelector('#score');
-var questContainer = document.querySelector('#question-container');
-var scoreContainer = document.querySelector('#score-container');
-var nameVal = document.querySelector('#name');
-var highScoreEl = document.querySelector('#high-score');
-var highScoreListEl = document.querySelector('#high-score-list');
+var viewScoreBtnEl = document.querySelector("#view-score");
+var timeEl = document.querySelector("#time-el");
+var startBtnEl = document.querySelector("#start-btn");
+var questionEl = document.querySelector("#question");
+var optA = document.querySelector("#a");
+var optB = document.querySelector("#b");
+var optC = document.querySelector("#c");
+var optD = document.querySelector("#d");
+var answerEl = document.querySelector("#answer");
+var allDone = document.querySelector("#all-done");
+var finalScoreEl = document.querySelector("#score");
+var questContainer = document.querySelector("#question-container");
+var scoreContainer = document.querySelector("#score-container");
+var nameVal = document.querySelector("#name");
+var highScoreEl = document.querySelector("#high-score");
+var highScoreListEl = document.querySelector("#high-score-list");
 var scoreRecords = [];
 var questions = [
-    {
-        question: 'JavaScript File Has An Extension of:',
-        optionA: 'A. .java',
-        optionB: 'B. .js',
-        optionC: 'C. javascript',
-        optionD: 'D. .xml',
-        correctAns: 'B'
-    },
-    {
-        question: 'Inside which HTML element do we put the JavaScript?',
-        optionA: 'A. <js>',
-        optionB: 'B. <javascript>',
-        optionC: 'C. <script>',
-        optionD: 'D. <scripting>',
-        correctAns: 'C'
-    },
-    {
-        question: 'Which Of The Dialog Box Display a Message And a Data Entry Field?',
-        optionA: 'A. Alert()',
-        optionB: 'B. Prompt()',
-        optionC: 'C. Confirm()',
-        optionD: 'D. Msg()',
-        correctAns: 'B'
-    },
-    {
-        question: 'How do you write "Hello World" in an alert box?',
-        optionA: 'A. msg("Hello World")',
-        optionB: 'B. msgBox("Hello World")',
-        optionC: 'C. alert("Hello World")',
-        optionD: 'D. alertBox("Hello World")',
-        correctAns: 'C'
-    },
-    {
-        question: 'How do you call a function named "myFunction"?',
-        optionA: 'A. call myFunction()',
-        optionB: 'B. call function myFunction()',
-        optionC: 'C. myFunction',
-        optionD: 'D. myFunction()',
-        correctAns: 'D'
-    },
+  {
+    question: "Why are flamingos pink?",
+    optionA: "A. There is no specific reason, they are just born that way.",
+    optionB: "B. Because of their shrimp-based diet",
+    optionC: "C. Because of a gene mutation",
+    optionD: "D. To stand out more in the wild ",
+    correctAns: "B",
+  },
+  {
+    question: "How many steps should you walk in a day?",
+    optionA: "A. 500",
+    optionB: "B. 6,000",
+    optionC: "C. 10,000",
+    optionD: "D. 8,000",
+    correctAns: "C",
+  },
+  {
+    question: "How much blood does a body of a grown-up approximately contain?",
+    optionA: "A. 7 litres",
+    optionB: "B. 5 litres",
+    optionC: "C. 3 litres",
+    optionD: "D. 9 litres",
+    correctAns: "B",
+  },
+  {
+    question:
+      "Which one is the only planet that orbits the sun in a clockwise direction?",
+    optionA: "A. Earth",
+    optionB: "B. Neptune",
+    optionC: "C. Venus",
+    optionD: "D. Uranus",
+    correctAns: "C",
+  },
+  {
+    question: "The apes that are the closest relatives to human beings are...",
+    optionA: "A. Orangutan and chimpanzee",
+    optionB: "B. Gorilla and orangutan",
+    optionC: "C. Gorilla and chimpanzee",
+    optionD: "D. Bonobo and chimpanzee",
+    correctAns: "D",
+  },
 ];
 function startQuiz() {
-    startBtnEl.addEventListener('click', timer);
-    displayQuestion();
-};
+  startBtnEl.addEventListener("click", timer);
+  displayQuestion();
+}
 
 startQuiz();
 
 function timer() {
-    var timerInterval = setInterval(function () {
-        startBtnEl.setAttribute('style', 'display : none')
-        questContainer.setAttribute('style', 'display: block');
-        if (secondsLeft > 0) {
-            secondsLeft--;
-            timeEl.textContent = 'Time: ' + secondsLeft + " second(s) left.";
-        } else {
-            clearInterval(timerInterval);
-            sendMessage();
-        }
-    }, 1000);
-};
+  var timerInterval = setInterval(function () {
+    startBtnEl.setAttribute("style", "display : none");
+    questContainer.setAttribute("style", "display: block");
+    if (secondsLeft > 0) {
+      secondsLeft--;
+      timeEl.textContent = "Time: " + secondsLeft + " second(s) left.";
+    } else {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+  }, 1000);
+}
 function sendMessage() {
-    timeEl.textContent = '';
-    allDone.textContent = 'ALL DONE!!!';
-    questContainer.setAttribute('style', 'display: none');
-    scoreContainer.setAttribute('style', 'display: block');
-    finalScoreEl.textContent = 'Your final score is: ' + finalScore;
-};
+  timeEl.textContent = "";
+  allDone.textContent = "ALL DONE!!!";
+  questContainer.setAttribute("style", "display: none");
+  scoreContainer.setAttribute("style", "display: block");
+  finalScoreEl.textContent = "Your final score is: " + finalScore;
+}
 function displayQuestion() {
-    optA.disabled = false;
-    optB.disabled = false;
-    optC.disabled = false;
-    optD.disabled = false;
-    questionEl.textContent = questions[counter].question;
-    optA.textContent = questions[counter].optionA;
-    optB.textContent = questions[counter].optionB;
-    optC.textContent = questions[counter].optionC;
-    optD.textContent = questions[counter].optionD;
-};
+  optA.disabled = false;
+  optB.disabled = false;
+  optC.disabled = false;
+  optD.disabled = false;
+  questionEl.textContent = questions[counter].question;
+  optA.textContent = questions[counter].optionA;
+  optB.textContent = questions[counter].optionB;
+  optC.textContent = questions[counter].optionC;
+  optD.textContent = questions[counter].optionD;
+}
 function getValue(selectedBtn) {
-    optA.disabled = true;
-    optB.disabled = true;
-    optC.disabled = true;
-    optD.disabled = true;
-    var selectedOptVal = selectedBtn;
-    var correctAnsVal = questions[counter].correctAns;
-    if (selectedOptVal === correctAnsVal) {
-        finalScore += 20;
-        answerEl.textContent = 'Correct!';
-    } else {
-        answerEl.textContent = 'Incorrect!';
-        secondsLeft = secondsLeft - 15;
-    };
-    if (counter < (questions.length - 1)) {
-        setTimeout(clearComment, 2000);
-        counter++;
-        setTimeout(displayQuestion, 2000);
-    } else {
-        secondsLeft = 0;
-        renderScores()
-    };
-};
+  optA.disabled = true;
+  optB.disabled = true;
+  optC.disabled = true;
+  optD.disabled = true;
+  var selectedOptVal = selectedBtn;
+  var correctAnsVal = questions[counter].correctAns;
+  if (selectedOptVal === correctAnsVal) {
+    finalScore += 20;
+    answerEl.textContent = "Correct!";
+  } else {
+    answerEl.textContent = "Incorrect!";
+    secondsLeft = secondsLeft - 15;
+  }
+  if (counter < questions.length - 1) {
+    setTimeout(clearComment, 2000);
+    counter++;
+    setTimeout(displayQuestion, 2000);
+  } else {
+    secondsLeft = 0;
+    renderScores();
+  }
+}
 function clearComment() {
-    answerEl.textContent = '';
-};
+  answerEl.textContent = "";
+}
 
 function submitRecord(event) {
-    event.preventDefault()
-    var currentScore = [nameVal.value, finalScore];
-    if (nameVal.value === '') {
-        alert('Please enter your name.')
-        return;
-    };
-    scoreRecords.push(currentScore);
-    storeScores();
-    viewHighScores();
-};
+  event.preventDefault();
+  var currentScore = [nameVal.value, finalScore];
+  if (nameVal.value === "") {
+    alert("Please enter your name.");
+    return;
+  }
+  scoreRecords.push(currentScore);
+  storeScores();
+  viewHighScores();
+}
 function storeScores() {
-    localStorage.setItem('score', JSON.stringify(scoreRecords));
-};
+  localStorage.setItem("score", JSON.stringify(scoreRecords));
+}
 function init() {
-    var storedScores = JSON.parse(localStorage.getItem('score'));
-    if (storedScores !== null) {
-        scoreRecords = storedScores;
-        console.log(scoreRecords);
-        renderScores();
-    };
-};
-function renderScores() {
-    var sortedScores = scoreRecords.sort((a, b) => b[1] - a[1]);
-    scoreContainer.setAttribute('style', 'display : none');
-    questContainer.setAttribute('style', 'display : none');
-    for (var i = 0; i < sortedScores.length; i++) {
-        var score = sortedScores[i];
-        var li = document.createElement('li');
-        li.textContent = score[0] + '  ---  ' + score[1];
-        console.log(score)
-        highScoreEl.append(li);
-        
-    }
-};
-function viewHighScores() {
-    highScoreListEl.setAttribute('style', 'display : block');
-    questContainer.setAttribute('style', 'display : none');
-    startBtnEl.setAttribute('style', 'display : none');
-    highScoreEl.setAttribute('style', 'display: block');
-    highScoreEl.innerHTML = '';
+  var storedScores = JSON.parse(localStorage.getItem("score"));
+  if (storedScores !== null) {
+    scoreRecords = storedScores;
+    console.log(scoreRecords);
     renderScores();
+  }
+}
+function renderScores() {
+  var sortedScores = scoreRecords.sort((a, b) => b[1] - a[1]);
+  scoreContainer.setAttribute("style", "display : none");
+  questContainer.setAttribute("style", "display : none");
+  for (var i = 0; i < sortedScores.length; i++) {
+    var score = sortedScores[i];
+    var li = document.createElement("li");
+    li.textContent = score[0] + "  ---  " + score[1];
+    console.log(score);
+    highScoreEl.append(li);
+  }
+}
+function viewHighScores() {
+  highScoreListEl.setAttribute("style", "display : block");
+  questContainer.setAttribute("style", "display : none");
+  startBtnEl.setAttribute("style", "display : none");
+  highScoreEl.setAttribute("style", "display: block");
+  highScoreEl.innerHTML = "";
+  renderScores();
 }
 init();
